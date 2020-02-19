4a330466870c248bfc52309de414bad3b5879fe6
Torjus Ødegård <torjusodegard@dhcp-10-22-1-145.wlan.ntnu.no>
2020-02-19 20:54:04 +0100

    Added PunishmentType model

--- backend/apps/punishment_system/models.py
+++ backend/apps/punishment_system/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class PunishmentType(models.Model):
+    name = models.CharField(max_length=50)
+    value = models.DecimalField(max_digits=6, decimal_places=2, default=0)
+
+    def __str__(self):
+        return f'Name: {self.name} Value: {self.value}'
