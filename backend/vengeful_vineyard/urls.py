f97eec068a12fff02136ca94bcf1c40573b13184
kharann <anhkhav@gmail.com>
2020-02-19 20:34:39 +0100

    add admin path to urls

--- backend/vengeful_vineyard/urls.py
+++ backend/vengeful_vineyard/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from vengeful_vineyard  import views
@@ -10,5 +11,6 @@ router.register(r'groups', views.GroupViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
+    path('admin/', admin.site.urls ),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
