523e84b89efdacd3cafba32fa210063878fe70b1
Joakim Fremstad <joa.fre@gmail.com>
2020-09-20 16:51:16 +0200

    Add deleted index

---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+//import ReactDOM from "react-dom";
+import App from "./_app";
+
+export default function Index() {
+  return (
+    <div>
+      <App />
+    </div>
+  );
+}
