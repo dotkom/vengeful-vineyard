25ea2f982214abef9d69a693a5c46ddbe53bdeca
Anna Irene Andresen <69513935+annaiandresen@users.noreply.github.com>
2020-11-16 16:56:12 +0100

    :sparkles: Add footer, body-width reduced (#46)
    * :sparkles: Add footer, body-width reduced
    
    * :sparkles: Add footer tag
    
    * :pencil2: remove script tags

---
+++ frontend/src/Components/Footer/FooterSign.svelte
@@ -0,0 +1,15 @@
+<div class="footer_sign">
+  Fra
+  <a href="mailto:dotkom@online.ntnu.no">Dotkom</a>
+  med
+  <span> <img src="assets/heart.svg" alt="heart" height="16px" /></span>
+</div>
+
+<style lang="less">
+  @import "../../variables.less";
+  .footer_sign {
+    font-size: 16px;
+    justify-content: center;
+    margin: 0;
+  }
+</style>
