59bd59eb8b519d87dd7b5fe6f861ca74c7df5f78
Joakim Fremstad <joa.fre@gmail.com>
2021-08-02 15:32:53 +0200

    Add more

--- backend/no_orm/main.py
+++ backend/no_orm/main.py
@@ -5,61 +5,82 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 
+class Group(BaseModel):
+    id: int
+    name: str
+    rules: str
+
+
 class User(BaseModel):
+    id: int
     first_name: str
     last_name: str
     age: int
     phone: str
 
 
-con = sqlite3.connect('example.db')
+con = sqlite3.connect("example.db")
 cur = con.cursor()
 with open("schema.sql", "r") as f:
     schema = f.readlines()
     schema = "".join([line.strip() for line in schema])
-cur.execute(schema)
+cur.executescript(schema)
 
 
-def dbUserJson(dbUser):
-    return {
-        "id": dbUser[0],
-        "first_name": dbUser[1],
-        "last_name": dbUser[2],
-        "age": dbUser[3],
-        "phone": dbUser[4],
-    }
+def dbToGroup(dbGroup) -> Group:
+    return Group(**dict(zip(Group.__fields__.keys(), dbGroup)))
+
+
+def dbToUser(dbUser) -> User:
+    return User(**dict(zip(User.__fields__.keys(), dbUser)))
+
+
+def insertUser(user: User):
+    statement = (
+        f"INSERT INTO users(first_name, last_name, age, phone) VALUES (?, ?, ?, ?)"
+    )
+    values = (user.first_name, user.last_name, user.age, user.phone)
+    try:
+        cur.execute(statement, values)
+    except sqlite3.IntegrityError:
+        raise HTTPException(status_code=400, detail="Database integrity violated")
+    con.commit()
+    return {"id": cur.lastrowid}
 
 
 app = FastAPI()
 
 
-@app.get("/user")
+@app.get("/user", tags=["User"])
 async def get_users():
     users = cur.execute("""SELECT * FROM users""")
-    return {"users": list(map(dbUserJson, users))}
+    return {"users": list(map(dbToUser, users))}
 
 
-@app.post("/user")
+@app.post("/user", tags=["User"])
 async def post_user(user: User):
     if user.age <= 18:
         raise HTTPException(status_code=400, detail="User is too young!")
-    statement = f"INSERT INTO users(first_name, last_name, age, phone) VALUES (?, ?, ?, ?)"
-    values = (user.first_name,
-              user.last_name, user.age, user.phone)
-    try:
-        cur.execute(statement, values)
-    except sqlite3.IntegrityError:
-        raise HTTPException(
-            status_code=400, detail="Database integrity violated")
-    con.commit()
-    return {"id": cur.lastrowid}
+    return insertUser(user)
 
 
-@app.get("/user/{user_id}")
-async def read_item(user_id: int):
+@app.get("/user/{user_id}", tags=["User"])
+async def get_user(user_id: int):
     user = cur.execute(
-        """SELECT * FROM users where user_id = :user_id""", {"user_id": user_id})
+        """SELECT * FROM users where user_id = :user_id""", {"user_id": user_id}
+    )
     user = user.fetchone()
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return dbUserJson(user)
+    return dbToUser(user)
+
+
+@app.get("/group/{group_id}", tags=["Group"])
+async def get_group(group_id: int):
+    group = cur.execute(
+        """SELECT * FROM groups where group_id = :group_id""", {"group_id": group_id}
+    )
+    group = group.fetchone()
+    if not group:
+        raise HTTPException(status_code=404, detail="Group not found")
+    return dbToGroup(group)
--- backend/no_orm/schema.sql
+++ backend/no_orm/schema.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS users (
 	age INTEGER NOT NULL,
 	phone TEXT NOT NULL UNIQUE
 );
+
+CREATE TABLE IF NOT EXISTS groups (
+	group_id INTEGER PRIMARY KEY,
+	name TEXT NOT NULL,
+	rules TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS group_members (
+	group_id INTEGER NOT NULL references groups(group_id),
+  user_id INTEGER NOT NULL references users(user_id),
+  is_admin BOOLEAN,
+  PRIMARY KEY (group_id, user_id)
+);
