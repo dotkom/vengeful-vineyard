dbae170e743cf22ed328807913dfb2935070d970
vigdisis <vigdis.steinsund@hotmail.com>
2021-11-07 14:48:51 +0100

    :sparkles: #19 add popup modal + input fields

--- frontend/src/Components/Top_group_picker_components/AddCustomGroup.svelte
+++ frontend/src/Components/Top_group_picker_components/AddCustomGroup.svelte
@@ -1,17 +1,74 @@
+
 <script lang="ts">
 
-    export let logoUrl = "";
-    export let name = "";
-  
+import Modal,{getModal} from './Modal.svelte'
+
+let modalTitle = 'Create new group';
+	
+	let selection
+
+  let groupName: string
+  let groupUsers: string[]
+  let groupUser: string
+
+    function setSelection(res){
+		selection=res
+	}
+
+  function validate() {
+		console.log("I'm the validate() function")
+	}
+   
   </script>
   
   <button 
     class="groupBtn" 
-    on:click={() => console.log("Clickd")}>
+    on:click={()=>getModal().open()}>
       <svg width="30" height="28" viewBox="0 0 30 28" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M18.2189 11.9844H26.841C28.1101 11.9844 29.1269 12.9825 29.1269 14.1986C29.1269 15.4146 28.1101 16.4127 26.841 16.4127H18.2189C17.8076 16.4127 17.4618 16.7386 17.4618 17.1556V25.5338C17.4618 26.7498 16.445 27.748 15.1759 27.748C13.9067 27.748 12.89 26.7498 12.89 25.5338V17.1556C12.89 16.7386 12.5441 16.4127 12.1328 16.4127H3.51075C2.24157 16.4127 1.22485 15.4146 1.22485 14.1986C1.22485 12.9825 2.24157 11.9844 3.51075 11.9844H12.1328C12.5441 11.9844 12.89 11.6585 12.89 11.2416V2.86335C12.89 1.6473 13.9067 0.64917 15.1759 0.64917C16.445 0.64917 17.4618 1.6473 17.4618 2.86335V11.2416C17.4618 11.6585 17.8076 11.9844 18.2189 11.9844Z" fill="#FBBF24" stroke="#EA9819" stroke-width="0.5"/>
     </svg>
   </button>
+
+<Modal>
+	<h1>{modalTitle}</h1>
+
+
+  <form on:submit|preventDefault={validate}>
+    <input type="text" bind:value={groupName} placeholder="Group name"/>
+
+    <div>
+    <input type="text" bind:value={groupUser} placeholder="User email"/>
+
+
+    <button >
+      Add user to group
+    </button>
+   
+
+
+
+  </div>
+
+  <textarea disabled={true} bind:value={groupUsers} ></textarea>
+
+  <button type=submit>
+		Create group
+	</button>
+  </form>
+  
+  
+  
+
+
+  <button>
+		Cancel
+	</button>
+	{#if selection}
+	<p>
+		Your selection was: {selection}
+	</p>
+	{/if}
+</Modal>
   
   <style lang="less">
     @import "../../variables.less";
@@ -42,5 +99,16 @@
     border: none;
     outline: none;
     }
+
+    form {
+      display: flex;
+      flex-direction: column;
+    }
+
+    input, textarea {
+      border: 1px solid #ccc;
+  border-radius: 4px;
+    }
+
   </style>
   
