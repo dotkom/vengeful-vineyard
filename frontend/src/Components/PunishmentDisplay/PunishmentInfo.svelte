d8c685a20c42df3f3fb7cceddba7e22104e9b13d
vigdisis <vigdis.steinsund@hotmail.com>
2022-03-05 17:22:04 +0100

    :construction: #23 Added UI dropdown for cancelling punishment

--- frontend/src/Components/PunishmentDisplay/PunishmentInfo.svelte
+++ frontend/src/Components/PunishmentDisplay/PunishmentInfo.svelte
@@ -128,14 +128,43 @@
           />
         {/each}
       </div>
-      <div class="col-span-2">
-        {returnDate(punishment.created_time)}
+      <div
+        class="col-span-2 relative h-full flex justify-center align-middle items-center"
+      >
+        <div class="dropdown dropdown-end absolute z-10 top-0 right-0">
+          <label
+            tabindex="0"
+            class="badge hover:cursor-pointer border-gray-300 bg-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-5 h-5 stroke-current"
+              ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path></svg
+            >
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content shadow bg-white rounded-box w-40"
+          >
+            <button>Annuler straff</button>
+          </ul>
+        </div>
+        <div>{returnDate(punishment.created_time)}</div>
       </div>
     </div>
   {:else}
     <div></div>
   {/each}
-  <div class="sum">Total sum: {totalSum}</div>
+  <div class="sum">
+    Total sum: {totalSum}
+  </div>
 </div>
 
 <style lang="less">
