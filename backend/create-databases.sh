c60e4a8e1afeb1707752b55b813a22da7e3c7579
Terbau <bragebau@hotmail.no>
2022-09-21 17:22:20 +0200

    Removed unnecessary comments part 2

--- backend/create-databases.sh
+++ backend/create-databases.sh
@@ -3,9 +3,6 @@
 set -e
 set -u
 
-# names=`ls ./tests/test*.py`
-
-# names=`ls ${PWD}/tests/test*.py`
 names=`ls /tests/test*.py`
 tomatch="class Test"
 amount=0
@@ -36,14 +33,5 @@ echo $amount
 if [ -n "$POSTGRES_DEFAULT_DATABASE_NAME" ]; then
 	for i in $(seq 1 $amount); do
 		create_user_and_database "${POSTGRES_DEFAULT_DATABASE_NAME}${i}"
-		# echo "${POSTGRES_DEFAULT_DATABASE_NAME}${i}"
 	done
 fi
-
-# if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
-# 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
-# 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-# 		create_user_and_database $db
-# 	done
-# 	echo "Multiple databases created"
-# fi
