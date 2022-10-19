44723ab2297ced91d520d2cbdaddb5f413a22060
VigdisIS <vigdis.steinsund@hotmail.com>
2022-10-19 20:17:27 +0200

    :bug: #140 add spinner

--- frontend/src/components/punishments/PunishmentsListed.svelte
+++ frontend/src/components/punishments/PunishmentsListed.svelte
@@ -4,6 +4,7 @@
   import { punishmentsToFilter } from "../../stores/punishmentToFilter";
   import { showPaid } from "../../stores/users";
   import { onlyShowAfterDate, onlyShowBeforeDate } from "../../stores/users";
+  import { Circle } from "svelte-loading-spinners";
 
   export let p_types: PunishmentType[];
 
@@ -15,7 +16,9 @@
 </script>
 
 <div class="flex justify-center flex-wrap max-w-[13rem]">
-  {#await row.straffer then}
+  {#await row.straffer}
+    <Circle size="60" color="#153E75" unit="px" duration="1s" />
+  {:then}
     {#if row.straffer}
       {#each row.straffer
         .filter((pun) => $punishmentsToFilter
