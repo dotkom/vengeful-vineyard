8d0b17a6c6c21cbafbb6e325b041cda747b83155
Anna Andresen <anna.irene.andresen@gmail.com>
2022-10-19 21:40:56 +0200

    :art: Move timefilter func to functions

--- frontend/src/lib/functions.ts
+++ frontend/src/lib/functions.ts
@@ -37,3 +37,20 @@ export const getLastPunishedDate = (user?: User): string => {
     return 'No date'
   }
 }
+
+export const shouldDisplay = (
+  dateGiven: Date,
+  onlyShowAfterDate: Date,
+  onlyShowBeforeDate: Date
+): boolean => {
+  return (
+    (dateGiven.getTime() >= onlyShowAfterDate.getTime() &&
+      new Date(dateGiven).getTime() <= onlyShowBeforeDate.getTime()) ||
+    (new Date(dateGiven).getDate() == onlyShowAfterDate.getDate() &&
+      new Date(dateGiven).getMonth() == onlyShowAfterDate.getMonth() &&
+      new Date(dateGiven).getFullYear() == onlyShowAfterDate.getFullYear()) ||
+    (new Date(dateGiven).getDate() == onlyShowBeforeDate.getDate() &&
+      new Date(dateGiven).getMonth() == onlyShowBeforeDate.getMonth() &&
+      new Date(dateGiven).getFullYear() == onlyShowBeforeDate.getFullYear())
+  )
+}
