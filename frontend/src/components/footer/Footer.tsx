416d50fc3b078783303ee965a3182c0952642054
isaksolheim <mail@isak.me>
2023-06-28 15:45:36 +0200

    Update footer design

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,22 +1,35 @@
-export const Footer = () => (
-  <footer className="text-center py-16">
-    <pre className="text-slate-600 mb-4 text-sm hidden md:block">
-      . . .. . *. - -_ _-__-0oOo _-_ -__ -||||) ______||||______ ~~~~~~~~~~`""'
-    </pre>
+import { GitHubIcon } from "../../assets/GitHubIcon";
 
-    <p>Laget for kulturen</p>
-    <p>
-      av{" "}
-      <a
-        href="https://github.com/dotkom/"
-        className="text-blue-500 hover:text-blue-600"
-      >
-        dotkom
-      </a>
-    </p>
+export const Footer = () => {
+  const links = [
+    {
+      name: "GitHub",
+      href: "https://github.com/dotkom/vengeful-vineyard",
+      icon: (props: React.ComponentProps<"svg">) => <GitHubIcon {...props} />,
+    },
+  ];
 
-    <pre className="text-slate-600 mt-4 text-sm hidden md:block">
-      . . .. . *. - -_ _-__-0oOo _-_ -__ -||||) ______||||______ ~~~~~~~~~~`""'
-    </pre>
-  </footer>
-);
+  return (
+    <footer className="bg-white">
+      <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+        <div className="flex justify-center space-x-6 md:order-2">
+          {links.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-gray-400 hover:text-gray-500"
+            >
+              <span className="sr-only">{item.name}</span>
+              <item.icon className="h-6 w-6" aria-hidden="true" />
+            </a>
+          ))}
+        </div>
+        <div className="mt-8 md:order-1 md:mt-0">
+          <p className="text-center text-xs leading-5 text-gray-500">
+            Laget av Dotkom
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
