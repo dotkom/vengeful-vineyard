d4dcfac5c6895cbff2cf1529b32cf189364ed606
isaksolheim <mail@isak.me>
2023-07-16 16:58:25 +0200

    Update footer color

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   ];
 
   return (
-    <footer className="bg-white">
+    <footer className="bg-gray-50 mt-8">
       <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {links.map((item) => (
