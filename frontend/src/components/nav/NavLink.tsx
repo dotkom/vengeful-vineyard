c61e00ceecb8b22e4f94b96e6fd5a97d65f5dfe4
isaksolheim <mail@isak.me>
2023-08-20 10:24:10 +0200

    Remove li wrapping navlink

--- frontend/src/components/nav/NavLink.tsx
+++ frontend/src/components/nav/NavLink.tsx
@@ -6,19 +6,17 @@ interface NavLinkProps {
 }
 
 export const NavLink = ({ label, url }: NavLinkProps) => (
-  <li>
-    <Link
-      className={({ isActive }) =>
-        `inline-flex items-center border-b-2 ${
-          isActive
-            ? "border-indigo-500 text-gray-900"
-            : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
-        } px-1 pt-1 text-sm font-medium `
-      }
-      to={url}
-      end
-    >
-      {label}
-    </Link>
-  </li>
+  <Link
+    className={({ isActive }) =>
+      `inline-flex items-center border-b-2 ${
+        isActive
+          ? "border-indigo-500 text-gray-900"
+          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700"
+      } px-1 pt-1 text-sm font-medium `
+    }
+    to={url}
+    end
+  >
+    {label}
+  </Link>
 );
