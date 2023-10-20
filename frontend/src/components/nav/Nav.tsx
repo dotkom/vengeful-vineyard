ba4bc7b6fd981b8301f6a9a7c5d1ba50661e35bb
jogt <jotjernshaugen@gmail.com>
2023-10-21 00:19:35 +0200

    Fixed formatting

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -12,65 +12,64 @@ interface NavProps {
   auth: AuthContextProps;
 }
 
-export const Nav = ({ auth }: NavProps) => {
-  return (
-    <Disclosure as="nav" className="bg-white shadow">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 justify-between">
-              <div className="flex">
-                <div className="-ml-2 mr-2 flex items-center md:hidden">
-                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Open main menu</span>
-                    {open ? (
-                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                    ) : (
-                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                    )}
-                  </Disclosure.Button>
-                </div>
-                <div className="flex flex-shrink-0 items-center">
-                  <Link to="/">
-                    <img
-                      className="block h-8 w-auto cursor-pointer lg:hidden"
-                      src={OnlineLogo}
-                      alt="Online Logo"
-                    />
-                  </Link>
-                  <Link to="/">
-                    <img
-                      className="hidden h-8 w-auto cursor-pointer lg:block"
-                      src={OnlineLogo}
-                      alt="Online Logo"
-                    />
-                  </Link>
-                </div>
-                <div className="hidden md:ml-6 md:flex md:space-x-8">
-                  <NavLink label="Hjem" url="/" />
-                  <NavLink label="Wall of Shame" url="/wall-of-shame" />
-                </div>
+export const Nav = ({ auth }: NavProps) => (
+  <Disclosure as="nav" className="bg-white shadow">
+    {({ open }) => (
+      <>
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex h-16 justify-between">
+            <div className="flex">
+              <div className="-ml-2 mr-2 flex items-center md:hidden">
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
               </div>
-              <div className="flex items-center">
-                <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="sr-only">Open user menu</span>
-                        <AvatarIcon className="h-10 w-10" />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-200"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {/*
+              <div className="flex flex-shrink-0 items-center">
+                <Link to="/">
+                  <img
+                    className="block h-8 w-auto cursor-pointer lg:hidden"
+                    src={OnlineLogo}
+                    alt="Online Logo"
+                  />
+                </Link>
+                <Link to="/">
+                  <img
+                    className="hidden h-8 w-auto cursor-pointer lg:block"
+                    src={OnlineLogo}
+                    alt="Online Logo"
+                  />
+                </Link>
+              </div>
+              <div className="hidden md:ml-6 md:flex md:space-x-8">
+                <NavLink label="Hjem" url="/" />
+                <NavLink label="Wall of Shame" url="/wall-of-shame" />
+              </div>
+            </div>
+            <div className="flex items-center">
+              <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
+                <Menu as="div" className="relative ml-3">
+                  <div>
+                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                      <span className="sr-only">Open user menu</span>
+                      <AvatarIcon className="h-10 w-10" />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-200"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      {/*
                       <Menu.Item>
                         {({ active }) => (
                           <Link
@@ -85,98 +84,98 @@ export const Nav = ({ auth }: NavProps) => {
                         )}
                       </Menu.Item>
                       */}
-                        <Menu.Item>
-                          {({ active }) => (
-                            <Fragment>
-                              {auth.isAuthenticated ? (
-                                <button
-                                  onClick={() => void auth.removeUser()}
-                                  className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
-                                  )}
-                                >
-                                  Logg ut
-                                </button>
-                              ) : (
-                                <button
-                                  onClick={() => void auth.signinRedirect()}
-                                  className={classNames(
-                                    active ? "bg-gray-100" : "",
-                                    "block px-4 py-2 text-sm text-gray-700"
-                                  )}
-                                >
-                                  Logg inn
-                                </button>
-                              )}
-                            </Fragment>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Fragment>
+                            {auth.isAuthenticated ? (
+                              <button
+                                onClick={() => void auth.removeUser()}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Logg ut
+                              </button>
+                            ) : (
+                              <button
+                                onClick={() => void auth.signinRedirect()}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block px-4 py-2 text-sm text-gray-700"
+                                )}
+                              >
+                                Logg inn
+                              </button>
+                            )}
+                          </Fragment>
+                        )}
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </div>
             </div>
           </div>
+        </div>
 
-          <Disclosure.Panel className="md:hidden">
-            <div className="space-y-1 pb-3 pt-2">
-              <Link to="/">
-                <Disclosure.Button
-                  as="span"
-                  className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
-                >
-                  Hjem
-                </Disclosure.Button>
-              </Link>
-              <Link to="/wall-of-shame">
-                <Disclosure.Button
-                  as="span"
-                  className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-                >
-                  Wall of Shame
-                </Disclosure.Button>
-              </Link>
-            </div>
-            <div className="border-t border-gray-200 pb-3 pt-4">
-              <div className="flex items-center px-4 sm:px-6">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src="https://media.licdn.com/dms/image/D4E03AQGhozXJkpG0JA/profile-displayphoto-shrink_800_800/0/1664545360034?e=2147483647&v=beta&t=AERDN5WsH6qFhpnUjD7dovDOtsOYJ7mQ0g0aXijSFQw"
-                    alt=""
-                  />
+        <Disclosure.Panel className="md:hidden">
+          <div className="space-y-1 pb-3 pt-2">
+            <Link to="/">
+              <Disclosure.Button
+                as="span"
+                className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700 sm:pl-5 sm:pr-6"
+              >
+                Hjem
+              </Disclosure.Button>
+            </Link>
+            <Link to="/wall-of-shame">
+              <Disclosure.Button
+                as="span"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              >
+                Wall of Shame
+              </Disclosure.Button>
+            </Link>
+          </div>
+          <div className="border-t border-gray-200 pb-3 pt-4">
+            <div className="flex items-center px-4 sm:px-6">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src="https://media.licdn.com/dms/image/D4E03AQGhozXJkpG0JA/profile-displayphoto-shrink_800_800/0/1664545360034?e=2147483647&v=beta&t=AERDN5WsH6qFhpnUjD7dovDOtsOYJ7mQ0g0aXijSFQw"
+                  alt=""
+                />
+              </div>
+              <div className="ml-3">
+                <div className="text-base font-medium text-gray-800">
+                  B-Rage
                 </div>
-                <div className="ml-3">
-                  <div className="text-base font-medium text-gray-800">
-                    B-Rage
-                  </div>
-                  <div className="text-sm font-medium text-gray-500">
-                    brage@online.ntnu.no
-                  </div>
+                <div className="text-sm font-medium text-gray-500">
+                  brage@online.ntnu.no
                 </div>
               </div>
-              <div className="mt-3 space-y-1">
+            </div>
+            <div className="mt-3 space-y-1">
+              <Link to="/profil">
                 <Disclosure.Button
                   as="span"
-                  onClick={() => navigate("/profil")}
                   className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
                 >
                   Profil
                 </Disclosure.Button>
-                <Disclosure.Button
-                  as="span"
-                  href="#"
-                  className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
-                >
-                  Logg ut
-                </Disclosure.Button>
-              </div>
+              </Link>
+              <Disclosure.Button
+                as="a"
+                href="#"
+                className="block px-4 py-2 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-800 sm:px-6"
+              >
+                Logg ut
+              </Disclosure.Button>
             </div>
-          </Disclosure.Panel>
-        </>
-      )}
-    </Disclosure>
-  );
-};
+          </div>
+        </Disclosure.Panel>
+      </>
+    )}
+  </Disclosure>
+);
