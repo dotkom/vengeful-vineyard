56e45e559e91e559e5c4cd95b7e9f574a59eacda
jogt <jotjernshaugen@gmail.com>
2023-10-25 16:10:00 +0200

    Added linting

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "prettier",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+  },
+  ignorePatterns: ["**/dist/**/*", "**/node_modules/**/*"],
+}
