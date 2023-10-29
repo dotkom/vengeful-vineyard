bff07d195f3e3f79c11e130aeed0e1d073ec5426
jogt <jotjernshaugen@gmail.com>
2023-10-29 21:33:20 +0100

    Removed test code

--- frontend/src/pages/wallOfShame/WallOfShame.tsx
+++ frontend/src/pages/wallOfShame/WallOfShame.tsx
@@ -3,19 +3,12 @@ import { Table } from "../../components/table"
 import axios, { AxiosResponse } from "axios"
 import { LEADERBOARD_URL } from "../../helpers/api"
 import { Leaderboard } from "../../helpers/types"
-import { useEffect } from "react"
 
 export const WallOfShame = () => {
   const { isLoading, data, refetch } = useQuery({
     queryKey: ["wallOfShame"],
     queryFn: () => axios.get(LEADERBOARD_URL).then((res: AxiosResponse<Leaderboard>) => res.data),
   })
-
-  useEffect(() => {
-    for (let i = 0; i < 2; i++) {
-      axios.get(LEADERBOARD_URL)
-    }
-  }, [])
 
   return (
     <section className="mt-16">
