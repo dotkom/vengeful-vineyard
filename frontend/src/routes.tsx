b39894bc6708146fec2a284416ad88cc7f682f32
jogt <jotjernshaugen@gmail.com>
2023-10-29 19:00:14 +0100

    Switch to hash router to avoid routing issues

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,10 @@
-import { createRoutesFromElements, createBrowserRouter, Route } from "react-router-dom"
+import { createRoutesFromElements, Route, createHashRouter } from "react-router-dom"
 import { Layout } from "./views/layout"
 import { Home } from "./pages/home"
 import { Profile } from "./pages/profile"
 import { WallOfShame } from "./pages/wallOfShame"
 
-export const router = createBrowserRouter(
+export const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<Home />} />
