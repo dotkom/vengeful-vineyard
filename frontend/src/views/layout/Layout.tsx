ce563767caa99efcf774b60c242c2ac8ce9f0e77
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-01-24 18:59:09 +0100

    Cook entire footer from monoweb (#296)
    * Cook entire footer from monoweb
    
    * add artifical margin to footer
    
    ---------
    
    Co-authored-by: Terbau <bragebau@hotmail.no>

--- frontend/src/views/layout/Layout.tsx
+++ frontend/src/views/layout/Layout.tsx
@@ -21,13 +21,15 @@ export const Layout = () => {
 
   if (auth.isLoading) {
     return (
-      <main className="flex h-screen flex-col bg-gray-50">
-        <Nav auth={auth} />
-        <div className="flex justify-center mt-16">
-          <Spinner />
-        </div>
+      <>
+        <main className="flex h-screen flex-col bg-gray-50">
+          <Nav auth={auth} />
+          <div className="flex justify-center mt-16">
+            <Spinner />
+          </div>
+        </main>
         <Footer />
-      </main>
+      </>
     )
   }
 
@@ -41,14 +43,16 @@ export const Layout = () => {
     }
 
     return (
-      <main className="flex h-screen flex-col justify-between bg-gray-50">
-        <Nav auth={auth} />
-        <div className="flex flex-col items-center justify-center mt-16">
-          <h1>Ai ai ai ai ai!!! En feil oppsto.</h1>
-          <p>Melding: {auth.error.message}</p>
-        </div>
+      <>
+        <main className="flex h-screen flex-col justify-between bg-gray-50">
+          <Nav auth={auth} />
+          <div className="flex flex-col items-center justify-center mt-16">
+            <h1>Ai ai ai ai ai!!! En feil oppsto.</h1>
+            <p>Melding: {auth.error.message}</p>
+          </div>
+        </main>
         <Footer />
-      </main>
+      </>
     )
   }
 
@@ -57,19 +61,22 @@ export const Layout = () => {
   }
 
   return (
-    <main className="flex h-full min-h-screen flex-col justify-between bg-gray-50">
-      <CurrentUserProvider>
-        <ConfirmModalProvider>
-          <NotificationProvider>
-            <Nav auth={auth} />
-            <div className="mb-auto">
-              <Outlet />
-            </div>
-            <Footer />
-            <Notification />
-          </NotificationProvider>
-        </ConfirmModalProvider>
-      </CurrentUserProvider>
-    </main>
+    <>
+      <main className="flex h-full min-h-screen flex-col justify-between bg-gray-50">
+        <CurrentUserProvider>
+          <ConfirmModalProvider>
+            <NotificationProvider>
+              <Nav auth={auth} />
+              <div className="mb-auto">
+                <Outlet />
+              </div>
+              <div className="h-16" />
+              <Notification />
+            </NotificationProvider>
+          </ConfirmModalProvider>
+        </CurrentUserProvider>
+      </main>
+      <Footer />
+    </>
   )
 }
