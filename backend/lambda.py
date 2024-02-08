bb58937acbfd2a88713b0f36c00607d60ba77d96
Mats Larsen <mats@jun.codes>
2024-02-08 13:46:51 +0100

    Remove AWS Lambda references and update image repository

--- backend/lambda.py
+++
@@ -1,5 +0,0 @@
-from mangum import Mangum
-
-from app.api.init_api import asgi_app
-
-handler = Mangum(asgi_app, lifespan="auto", api_gateway_base_path="/api")
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 doppler run pnpm build
-aws s3 rm s3://dev.redwine-static.online.ntnu.no/ --recursive
-aws s3 cp dist s3://dev.redwine-static.online.ntnu.no/ --recursive
-aws cloudfront create-invalidation --distribution-id EFTDBFRP1Q91I --paths '/*'
+aws s3 rm s3://dev.redwine.online.ntnu.no/ --recursive
+aws s3 cp dist s3://dev.redwine.online.ntnu.no/ --recursive
+aws cloudfront create-invalidation --distribution-id E2K89BLF8EKZYA --paths '/*'
