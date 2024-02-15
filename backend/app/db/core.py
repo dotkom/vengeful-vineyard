7e820b369e30918cdaa314eda56f77f98568f7cf
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-15 18:45:04 +0100

    Fixed migrator

--- backend/app/db/core.py
+++ backend/app/db/core.py
@@ -137,23 +137,23 @@ class Database:
         def get_version_from_name(name: str) -> int:
             return int(name.split("_", 1)[0])
 
-        file_ = max(
+        files = sorted(
             (f for f in settings.migrations_directory.iterdir() if f.suffix == ".sql"),
             key=lambda f: get_version_from_name(f.name),
         )
 
         async with MaybeAcquire(conn, self.pool) as conn:
             schema_version = await self.get_migration_lock_version(conn)
-
             logger.debug("Schema version: %d", schema_version)
 
-            file_version = get_version_from_name(file_.name)
-            if file_version <= schema_version:
-                logger.debug("Skipping migration: %s", file_.name)
-                return
+            for file in files:
+                file_version = get_version_from_name(file.name)
+                if file_version <= schema_version:
+                    logger.debug("Skipping migration: %s", file.name)
+                    continue
 
-            sql_commands = read_sql_file(file_)
-            logger.info("Applying migration: %s", file_.name)
+                sql_commands = read_sql_file(file)
+                logger.info("Applying migration: %s", file.name)
 
-            await conn.execute(sql_commands)
-            await self.set_migration_lock_version(conn, file_version)
+                await conn.execute(sql_commands)
+                await self.set_migration_lock_version(conn, file_version)
