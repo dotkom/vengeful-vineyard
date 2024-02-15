76ce6f2e22a5002086df6023a9e5db65394b2cd6
Andre Klarpås <andre.klarpaas@gmail.com>
2024-02-15 21:43:18 +0100

    Hide groups without images from statistics

--- frontend/src/components/committees/CommitteeList.tsx
+++ frontend/src/components/committees/CommitteeList.tsx
@@ -4,14 +4,18 @@ import { GroupStatistics } from "../../helpers/types"
 
 export const CommitteeList = () => {
   const { data: groupsStatistics } = useCommittees()
+
+  const filteredGroupsStatistics = groupsStatistics?.filter(
+    (groupStatistic) => groupStatistic.group_image && groupStatistic.group_image != "NoImage"
+  )
   return (
     <div className="flex flex-wrap -m-2 gap-16 justify-center">
-      {groupsStatistics?.length == 0 ? (
+      {filteredGroupsStatistics?.length == 0 ? (
         <div className="flex flex-col items-center justify-center p-4">
           <p className="text-gray-800">Ingen resultat</p>
         </div>
       ) : (
-        groupsStatistics?.map((groupStatistic: GroupStatistics, index) => (
+        filteredGroupsStatistics?.map((groupStatistic: GroupStatistics, index) => (
           <CommitteeCard
             key={index}
             name={groupStatistic.group_name_short}
