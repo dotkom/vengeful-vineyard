3e4a7373d1e8971860cd62ac1d5b2e21becd1a1b
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-15 18:15:00 +0100

    Ran prettier on files

--- frontend/src/components/committees/CommitteeCard.tsx
+++ frontend/src/components/committees/CommitteeCard.tsx
@@ -4,12 +4,12 @@ interface CommitteeCardProps {
   imgurl: string
 }
 
-export const CommitteeCard = ({name, amount, imgurl} : CommitteeCardProps) => (
-    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-60 h-[17rem]">
-      <div className="mb-4">
-        <img className="h-32 w-32" src={imgurl} alt={name + " Logo"} />
-      </div>
-      <span className="block text-lg font-bold">{name}</span>
-      <span className="block text-lg font-bold">{amount},-</span>
+export const CommitteeCard = ({ name, amount, imgurl }: CommitteeCardProps) => (
+  <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-60 h-[17rem]">
+    <div className="mb-4">
+      <img className="h-32 w-32" src={imgurl} alt={name + " Logo"} />
     </div>
+    <span className="block text-lg font-bold">{name}</span>
+    <span className="block text-lg font-bold">{amount},-</span>
+  </div>
 )
--- frontend/src/components/committees/CommitteeList.tsx
+++ frontend/src/components/committees/CommitteeList.tsx
@@ -3,20 +3,23 @@ import { useCommittees } from "../../helpers/api"
 import { GroupStatistics } from "../../helpers/types"
 
 export const CommitteeList = () => {
-    const { data: groupsStatistics } = useCommittees()
-    return (
-        <div className="flex flex-wrap -m-2 gap-16 justify-center">
-            {
-                groupsStatistics?.length == 0 ? (
-                    <div className="flex flex-col items-center justify-center p-4">
-                        <p className="text-gray-800">Ingen resultat</p>
-                    </div>
-                ) : (
-                    groupsStatistics?.map((groupStatistic : GroupStatistics, index) => (
-                        <CommitteeCard key={index} name={groupStatistic.group_name_short} amount={groupStatistic.total_value} imgurl={groupStatistic.group_image} />
-                    ))
-                )
-            }
+  const { data: groupsStatistics } = useCommittees()
+  return (
+    <div className="flex flex-wrap -m-2 gap-16 justify-center">
+      {groupsStatistics?.length == 0 ? (
+        <div className="flex flex-col items-center justify-center p-4">
+          <p className="text-gray-800">Ingen resultat</p>
         </div>
-    )
+      ) : (
+        groupsStatistics?.map((groupStatistic: GroupStatistics, index) => (
+          <CommitteeCard
+            key={index}
+            name={groupStatistic.group_name_short}
+            amount={groupStatistic.total_value}
+            imgurl={groupStatistic.group_image}
+          />
+        ))
+      )}
+    </div>
+  )
 }
