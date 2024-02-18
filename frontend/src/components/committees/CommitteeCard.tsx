e4456c538face3df85d824875a6be48b70105668
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-18 22:05:29 +0100

    Made lightmode also grayscale committee logos

--- frontend/src/components/committees/CommitteeCard.tsx
+++ frontend/src/components/committees/CommitteeCard.tsx
@@ -7,7 +7,7 @@ interface CommitteeCardProps {
 export const CommitteeCard = ({ name, amount, imgurl }: CommitteeCardProps) => (
   <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-60 h-[17rem]">
     <div className="mb-4">
-      <img className="h-32 w-32 dark:grayscale dark:invert dark:contrast-200" src={imgurl} alt={name + " Logo"} />
+      <img className="h-32 w-32 grayscale dark:invert dark:contrast-200" src={imgurl} alt={name + " Logo"} />
     </div>
     <span className="block text-lg font-bold text-black">{name}</span>
     <span className="block text-lg font-bold text-black">{amount},-</span>
