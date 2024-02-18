ad9bdf25f162fd0c86352fc2fde715dab681af3e
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-18 22:40:54 +0100

    Added fallback group icon for group statistics instead of filtering missing images

--- frontend/src/components/committees/CommitteeCard.tsx
+++ frontend/src/components/committees/CommitteeCard.tsx
@@ -1,13 +1,13 @@
 interface CommitteeCardProps {
   name: string
   amount: number
-  imgurl: string
+  img: string | JSX.Element
 }
 
-export const CommitteeCard = ({ name, amount, imgurl }: CommitteeCardProps) => (
+export const CommitteeCard = ({ name, amount, img }: CommitteeCardProps) => (
   <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col items-center w-60 h-[17rem]">
     <div className="mb-4">
-      <img className="h-32 w-32 grayscale dark:invert dark:contrast-200" src={imgurl} alt={name + " Logo"} />
+      <img className="h-32 w-32 grayscale dark:invert dark:contrast-200" src={img} alt={name + " Logo"} />
     </div>
     <span className="block text-lg font-bold text-black">{name}</span>
     <span className="block text-lg font-bold text-black">{amount},-</span>
--- frontend/src/components/committees/CommitteeList.tsx
+++ frontend/src/components/committees/CommitteeList.tsx
@@ -2,27 +2,25 @@ import { CommitteeCard } from "./CommitteeCard"
 import { useAuth } from "react-oidc-context"
 import { useCommittees } from "../../helpers/api"
 import { GroupStatistics } from "../../helpers/types"
+import NoImage from "../../assets/NoImage.png"
 
 export const CommitteeList = () => {
   const auth = useAuth()
   const { data: groupsStatistics } = useCommittees({ enabled: auth.isAuthenticated })
 
-  const filteredGroupsStatistics = groupsStatistics?.filter(
-    (groupStatistic) => groupStatistic.group_image && groupStatistic.group_image != "NoImage"
-  )
   return (
     <div className="flex flex-wrap -m-2 gap-16 justify-center">
-      {filteredGroupsStatistics?.length == 0 ? (
+      {groupsStatistics?.length == 0 ? (
         <div className="flex flex-col items-center justify-center p-4">
           <p className="text-gray-800">Ingen resultat</p>
         </div>
       ) : (
-        filteredGroupsStatistics?.map((groupStatistic: GroupStatistics, index) => (
+        groupsStatistics?.map((groupStatistic: GroupStatistics, index) => (
           <CommitteeCard
             key={index}
             name={groupStatistic.group_name_short}
             amount={groupStatistic.total_value}
-            imgurl={groupStatistic.group_image}
+            img={groupStatistic.group_image === "NoImage" ? NoImage : groupStatistic.group_image}
           />
         ))
       )}
