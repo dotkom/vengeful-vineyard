cf81780d82879a90fdd6686a3bdd029d3f620a72
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-21 23:03:04 +0100

    Fixed deploy script to work in prod and staging

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+  echo "No environment specified, defaulting to staging"
+  ENVIRONMENT="staging"
+else
+  ENVIRONMENT=$1
+fi
+
+doppler setup -c $ENVIRONMENT -p vengeful-vineyard
+
 doppler run pnpm build
-aws s3 rm s3://dev.vinstraff.no/ --recursive
-aws s3 cp dist s3://dev.vinstraff.no/ --recursive
-aws cloudfront create-invalidation --distribution-id EJKLH539HRJU2 --paths '/*'
+
+FRONTEND_S3_BUCKET=$(doppler run --command 'echo $FRONTEND_S3_BUCKET')
+aws s3 rm $FRONTEND_S3_BUCKET --recursive
+aws s3 cp dist $FRONTEND_S3_BUCKET --recursive
+# aws cloudfront create-invalidation --distribution-id EJKLH539HRJU2 --paths '/*'
