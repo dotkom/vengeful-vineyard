5512fe7d61fc3ebc74db9f8fea586e859b759791
Mats Jun <mats@jun.codes>
2024-02-22 11:54:58 +0100

    Fix scripts for production and staging deployment (#319)

--- backend/lightsail-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-aws lightsail create-container-service-deployment\
-	--service-name vengeful-server-dev\
-	--containers "$(aws lightsail get-container-service-deployments --service-name vengeful-server-dev | jq --compact-output --raw-output '[.deployments[] | select(.state == "ACTIVE")] | max_by(.version) | .containers')"\
-	--public-endpoint "$(aws lightsail get-container-service-deployments --service-name vengeful-server-dev | jq --compact-output --raw-output '[.deployments[] | select(.state == "ACTIVE")] | max_by(.version) | .publicEndpoint')"
