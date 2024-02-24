107dafdb3d60482335d0519f4b66c81280a90dc0
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-02-24 19:48:59 +0100

    Darkmode fix for group login page

--- frontend/src/views/LoginRegisterPromptPage.tsx
+++ frontend/src/views/LoginRegisterPromptPage.tsx
@@ -9,7 +9,7 @@ export default function LoginRegisterPromptPage() {
 
   return (
     <div className="flex flex-col items-center justify-center space-y-4 p-40">
-      <h1 className="text-6xl font-bold text-center">Logg inn for å se denne siden!</h1>
+      <h1 className="text-6xl font-bold text-center text-black dark:text-white">Logg inn for å se denne siden!</h1>
       <p className="text-center text-lg font-medium text-gray-600">
         Dersom du ikke har en Onlineweb-konto opprett en først.
       </p>
