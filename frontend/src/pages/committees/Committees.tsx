3c6b9e14443d47f8647b6eb085418c03fd394681
Andre Klarpås <andre.klarpaas@gmail.com>
2024-03-17 21:55:20 +0100

    Added max width on statistics page

--- frontend/src/pages/committees/Committees.tsx
+++ frontend/src/pages/committees/Committees.tsx
@@ -2,7 +2,7 @@ import { CommitteeList } from "../../components/committees/CommitteeList"
 
 export const Committees = () => {
   return (
-    <section className="mt-8 mb-8 md:mt-16 max-w-8xl w-[90%] mx-auto">
+    <section className="mt-8 mb-8 md:mt-16 max-w-screen-xl w-[90%] mx-auto">
       <h1 className="mb-14 text-center md:text-xl font-medium text-black">Total straffesum per komité</h1>
       <CommitteeList />
     </section>
