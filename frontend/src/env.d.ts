84e494b9bd7b0f80ddea2b89b14ee56b44fb3628
Jo Gramnæs Tjernshaugen <jotjernshaugen@gmail.com>
2024-03-23 13:47:51 +0100

    Add sentry to frontend

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,8 +1,8 @@
-// NB: Also set in env.ts
-
 interface ImportMetaEnv {
   VITE_REDIRECT_URI: string
   AUTH0_CLIENT_ID: string
   AUTH0_ISSUER: string
   VITE_API_URL: string
+  SENTRY_ENVIRONMENT: string
+  SENTRY_FRONTEND_DSN: string
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ["VITE_", "AUTH0_"],
+  envPrefix: ["VITE_", "AUTH0_", "SENTRY_"],
   server: {
     port: 3000,
   },
