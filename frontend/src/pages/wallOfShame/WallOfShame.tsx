c805ff37a54875108113aa0e239cf0de7db43411
Andre Klarpås <andre.klarpaas@gmail.com>
2024-03-28 20:28:06 +0100

    Fix show if current user is the one who reacted in wall of shame

--- frontend/src/pages/wallOfShame/WallOfShame.tsx
+++ frontend/src/pages/wallOfShame/WallOfShame.tsx
@@ -1,9 +1,21 @@
 import { LeaderboardTable } from "../../components/leaderboardtable"
-import { useInfiniteQuery } from "@tanstack/react-query"
-import { leaderboardQuery } from "../../helpers/api"
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query"
+import { leaderboardQuery, userQuery } from "../../helpers/api"
 import { Button } from "../../components/button/Button"
+import { useCurrentUser } from "../../helpers/context/currentUserContext"
 
 export const WallOfShame = () => {
+  const { setCurrentUser } = useCurrentUser()
+
+  useQuery({
+    ...userQuery(),
+    onSuccess: ({ user_id }) => {
+      if (!user_id) return
+
+      setCurrentUser({ user_id })
+    },
+  })
+
   const { isFetching, data, refetch, fetchNextPage } = useInfiniteQuery(leaderboardQuery())
 
   const leaderboardUsers = data?.pages.flatMap((page) => page.results)
@@ -11,11 +23,7 @@ export const WallOfShame = () => {
   return (
     <section className="mt-8 md:mt-16 max-w-5xl w-[90%] mx-auto">
       <h1 className="mb-4 text-center md:text-xl font-medium text-black">Wall of Shame</h1>
-      <LeaderboardTable
-        leaderboardUsers={leaderboardUsers}
-        isFetching={isFetching}
-        dataRefetch={refetch}
-      />
+      <LeaderboardTable leaderboardUsers={leaderboardUsers} isFetching={isFetching} dataRefetch={refetch} />
       <Button variant="OUTLINE" onClick={() => fetchNextPage().then()} className="mx-auto mt-4">
         Last inn mer
       </Button>
