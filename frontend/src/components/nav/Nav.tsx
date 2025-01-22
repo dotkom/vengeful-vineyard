df6d6a0b88e8ffcb47c3226afb3c0a27926baa15
Julian Ammouche Ottosen <42567826+julian-ao@users.noreply.github.com>
2025-01-22 17:31:07 +0100

    fix: made the whole dropdown-buttons clickable (#371)

--- frontend/src/components/nav/Nav.tsx
+++ frontend/src/components/nav/Nav.tsx
@@ -158,13 +158,16 @@ export const Nav = ({ auth }: NavProps) => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white  py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-700 ">
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-700">
                       <Menu.Item>
                         {({ active }) => (
                           <Fragment>
                             <button
                               onClick={() => setDarkMode(!darkMode)}
-                              className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm")}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block w-full text-left px-4 py-2 text-sm"
+                              )}
                             >
                               {darkMode ? "Light mode" : "Dark mode"}
                             </button>
@@ -175,14 +178,20 @@ export const Nav = ({ auth }: NavProps) => {
                                     navigate("/")
                                   })
                                 }}
-                                className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm")}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block w-full text-left px-4 py-2 text-sm"
+                                )}
                               >
                                 Logg ut
                               </button>
                             ) : (
                               <button
                                 onClick={() => void auth.signinRedirect()}
-                                className={classNames(active ? "bg-gray-100" : "", "block px-4 py-2 text-sm")}
+                                className={classNames(
+                                  active ? "bg-gray-100" : "",
+                                  "block w-full text-left px-4 py-2 text-sm"
+                                )}
                               >
                                 Logg inn
                               </button>
