65d5df65775a212a6bae1daf41705216623ada78
Jonas <jonas@jole.xyz>
2025-03-18 15:33:57 +0100

    flip format (#380)

--- frontend/src/components/punishment/PunishmentItem.tsx
+++ frontend/src/components/punishment/PunishmentItem.tsx
@@ -91,7 +91,7 @@ export const PunishmentItem = ({
 
   const date = dayjs.utc(punishment.created_at).tz("Europe/Oslo")
 
-  const formattedDate = date.format("kl. HH:mm, DD. MMM YYYY")
+  const formattedDate = date.format("DD. MMM YYYY, 20:17")
 
   const isWallOfShame = !isGroupContext
 
